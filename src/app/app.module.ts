13b27af0a70efdd51eb7cd7f43bbfa31be0c42b0
Danny Cai <Danny.Cai@melanoma.org.au>
2024-01-02 10:49:34 +1100

    create components for players and courts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlayerComponent } from './player/player.component';
+import { CourtComponent } from './court/court.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PlayerComponent, CourtComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
