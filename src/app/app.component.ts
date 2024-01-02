7d619f4b008af06c35c8e8fd0dcb9613e3aa26ee
Danny Cai <Danny.Cai@melanoma.org.au>
2024-01-02 14:47:24 +1100

    set up routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'badmintonapp';
-  // waiting players
-  // courts
-  // all players
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlayerComponent } from './player/player.component';
 import { CourtComponent } from './court/court.component';
+import { PlayerControllerComponent } from './player-controller/player-controller.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, PlayerComponent, CourtComponent],
+  declarations: [AppComponent, PlayerComponent, CourtComponent, PlayerControllerComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
