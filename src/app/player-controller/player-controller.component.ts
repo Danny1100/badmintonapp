99072dd529bf9343920bb04e479ae006f9c8529f
Danny Cai <Danny.Cai@melanoma.org.au>
2024-01-03 13:54:24 +1100

    WIP add player page

--- src/app/player-controller/player-controller.component.ts
+++ src/app/player-controller/player-controller.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  Player,
+  PlayerService,
+  PlayerSkillData,
+} from '../player/services/player.service';
 
 @Component({
   selector: 'app-player-controller',
   templateUrl: './player-controller.component.html',
-  styleUrls: ['./player-controller.component.css']
+  styleUrls: ['./player-controller.component.css'],
 })
 export class PlayerControllerComponent {
+  skillLevels: PlayerSkillData[] = [];
+  addedPlayer!: Player;
 
+  getRadioStyle(playerSkillLevel: PlayerSkillData) {
+    return {
+      width: '150px',
+      border: `1px solid ${playerSkillLevel.colour}`,
+    };
+  }
+
+  constructor(private playerService: PlayerService) {}
+
+  ngOnInit() {
+    this.skillLevels = this.playerService.getSkillMap();
+  }
 }
--- src/app/player/services/player.service.ts
+++ src/app/player/services/player.service.ts
@@ -1,19 +1,61 @@
 import { Injectable } from '@angular/core';
 
+export interface Player {
+  name: string;
+  skillId: 0 | 1 | 2 | 3 | 4;
+}
+enum PlayerSkillLevelDesc {
+  Cracked = 'cracked',
+  Strong = 'strong',
+  Good = 'good',
+  Amateur = 'amateur',
+  New = 'new',
+}
+enum PlayerSkillLevelColour {
+  Cracked = '#f30da9',
+  Strong = '#f30d0d',
+  Good = '#f3af0d',
+  Amateur = '#0db2f3',
+  New = '#e1f30d',
+}
+export interface PlayerSkillData {
+  skillLevel: PlayerSkillLevelDesc;
+  colour: PlayerSkillLevelColour;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerService {
   // player skill level - lower number/index is higher skill
   skillMap = [
-    { skillLevel: 'cracked', colour: '#f30da9' },
-    { skillLevel: 'strong', colour: '#f30d0d' },
-    { skillLevel: 'good', colour: '#f3af0d' },
-    { skillLevel: 'amateur', colour: '#0db2f3' },
-    { skillLevel: 'new', colour: '#e1f30d' },
+    {
+      skillLevel: PlayerSkillLevelDesc.Cracked,
+      colour: PlayerSkillLevelColour.Cracked,
+    },
+    {
+      skillLevel: PlayerSkillLevelDesc.Strong,
+      colour: PlayerSkillLevelColour.Strong,
+    },
+    {
+      skillLevel: PlayerSkillLevelDesc.Good,
+      colour: PlayerSkillLevelColour.Good,
+    },
+    {
+      skillLevel: PlayerSkillLevelDesc.Amateur,
+      colour: PlayerSkillLevelColour.Amateur,
+    },
+    {
+      skillLevel: PlayerSkillLevelDesc.New,
+      colour: PlayerSkillLevelColour.New,
+    },
   ];
 
   constructor() {}
+
+  getSkillMap() {
+    return this.skillMap;
+  }
 
   getPlayerDetails(skillId: number) {
     return this.skillMap[skillId];
