940f1d3b12fa960e860a9b3bdeeb23fa46054505
Danny Cai <Danny.Cai@melanoma.org.au>
2024-01-09 11:21:42 +1100

    WIP matchmaking

--- src/app/matchmaking/matchmaking.service.ts
+++ src/app/matchmaking/matchmaking.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, ReplaySubject } from 'rxjs';
-import { Player } from '../player/services/player.service';
+import {
+  Player,
+  PlayerSkillLevelDesc,
+} from '../player/services/player.service';
 import { PlayerListService } from '../player-list/player-list.service';
 import { CourtControllerService } from '../court-controller/court-controller.service';
 import { Court } from '../court/court.component';
@@ -19,6 +22,7 @@ export class MatchmakingService {
   courtList$: BehaviorSubject<Court[]> =
     this.courtControllerService.getCourts();
   courtList!: Court[];
+  waitingGroups$: ReplaySubject<Court[]> = new ReplaySubject<Court[]>();
 
   getWaitingPlayers() {
     return this.waitingPlayers$;
@@ -28,7 +32,39 @@ export class MatchmakingService {
     waitingPlayers = waitingPlayers.filter((player) => player.id !== playerId);
     this.waitingPlayers$.next(waitingPlayers);
   }
-  matchmake(waitingPlayers: Player[]) {}
+  matchmake(court: Court, waitingPlayers: Player[]) {
+    // use a dictionary to store the number of players in each level
+    const skillLevels = Object.keys(PlayerSkillLevelDesc);
+    const playerSkillMap = new Map();
+    skillLevels.forEach((skillLevelDesc, i) => playerSkillMap.set(i, []));
+    waitingPlayers.forEach((player) => {
+      const { skillId } = player;
+      const currentList = playerSkillMap.get(skillId);
+      playerSkillMap.set(skillId, [...currentList, player]);
+    });
+    // group similar skill players together in groups of 4 by iterating through the map
+    const sortedPlayerQueue: Player[] = [];
+    Array.from(playerSkillMap.values()).forEach((playerGroup) =>
+      playerGroup.forEach((player: Player) => sortedPlayerQueue.push(player)),
+    );
+    const waitingGroups: Court[] = [];
+    let courtPlayers: Player[] = [];
+    let count = 0;
+    sortedPlayerQueue.forEach((player) => {
+      courtPlayers.push(player);
+      count++;
+      if (count % 4 === 0) {
+        waitingGroups.push({
+          courtNumber: court.courtNumber,
+          players: courtPlayers,
+        });
+        courtPlayers = [];
+      }
+    });
+    console.log(waitingGroups);
+    this.waitingGroups$.next(waitingGroups);
+    // whoever is highest in the waiting list in each group will determine how early they are in the group queue
+  }
   cycleCourt(court: Court) {
     // if there are players on the court, move them all to the bottom of the waiting players list and update the court list
     if (court.players.length > 0) {
@@ -39,15 +75,14 @@ export class MatchmakingService {
       this.courtControllerService.updateCourt({ ...court, players: [] });
       return;
     }
-    // if not, run matchmaking algorithm, remove them from the waiting players list, and put them in a court
-    // when to run matchmaking algorithm? every time a new player is added from any source?
+    // if not, run matchmaking algorithm and put them in the court, remove them from the waiting players list
+    // run matchmaking algorithm every time a new player is added from any source
     const waitingPlayers = this.waitingPlayers$.getValue();
     if (waitingPlayers.length < 4) {
       alert('Not enough players to matchmake');
       return;
     }
-    const nextPlayers = this.matchmake(waitingPlayers);
-    // what happens when you remove a court with existing players - players should be moved to waiting players list
+    this.matchmake(court, waitingPlayers);
   }
 
   constructor(
