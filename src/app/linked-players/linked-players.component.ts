f92a4e9920e6e3f3ae122b401a6d2ef95ae0c853
Danny Cai <Danny.Cai@melanoma.org.au>
2024-01-29 15:19:11 +1100

    refresh on linked players page now has warning

--- src/app/linked-players/linked-players.component.ts
+++ src/app/linked-players/linked-players.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-linked-players',
   templateUrl: './linked-players.component.html',
-  styleUrls: ['./linked-players.component.css']
+  styleUrls: ['./linked-players.component.css'],
 })
 export class LinkedPlayersComponent {
-
+  @HostListener('window:beforeunload', ['$event']) onRefresh(event: Event) {
+    event.returnValue = false;
+  }
 }
