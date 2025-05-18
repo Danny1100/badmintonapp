78413bad52614a230eb699289f1089cb335e53c3
Danny Cai <dannycai1100@gmail.com>
2025-05-18 16:52:09 +1000

    add player-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { AddPlayerComponent } from './add-player/add-player.component';
 import { LinkedPlayersComponent } from './linked-players/linked-players.component';
 import { WaitDurationComponent } from './wait-duration/wait-duration.component';
+import { PlayerListComponent } from './player-list/player-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'add-players', component: AddPlayerComponent },
+  { path: 'player-list', component: PlayerListComponent },
   { path: 'linked-players', component: LinkedPlayersComponent },
   { path: 'wait-duration', component: WaitDurationComponent },
 ];
