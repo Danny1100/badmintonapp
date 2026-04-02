9ed30d072d974ddf1f66197e061320c89302d657
Danny <dannycai1100@gmail.com>
2026-04-02 11:38:24 +1100

    Revert "add court timer"
    This reverts commit 86591b2269d131a70b99807aaf2e97f4ca093628.

--- src/app/court/court.component.ts
+++ src/app/court/court.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnDestroy,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Player } from '../player/services/player.service';
 import { CourtControllerService } from '../court-controller/court-controller.service';
 import { MatchmakingService } from '../matchmaking/matchmaking.service';
@@ -22,53 +16,16 @@ export interface Court {
   standalone: true,
   imports: [PlayerComponent],
 })
-export class CourtComponent implements OnChanges, OnDestroy {
+export class CourtComponent {
   @Input({ required: true }) courtNumber!: number;
   @Input({ required: true }) players!: Player[];
   @Input() showButtons = true;
   disableNextCourtButton = true;
 
-  elapsedSeconds = 0;
-  private timerInterval: ReturnType<typeof setInterval> | null = null;
-
   constructor(
     private courtControllerService: CourtControllerService,
     private matchmakingService: MatchmakingService,
   ) {}
-
-  get stopwatchDisplay(): string {
-    const minutes = Math.floor(this.elapsedSeconds / 60);
-    const seconds = this.elapsedSeconds % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['players']) {
-      const players: Player[] = changes['players'].currentValue;
-      if (players.length > 0 && !this.timerInterval) {
-        this.startTimer();
-      } else if (players.length === 0) {
-        this.resetTimer();
-      }
-    }
-  }
-
-  private startTimer() {
-    this.elapsedSeconds = 0;
-    this.timerInterval = setInterval(() => this.elapsedSeconds++, 1000);
-  }
-
-  private resetTimer() {
-    if (this.timerInterval) {
-      clearInterval(this.timerInterval);
-      this.timerInterval = null;
-    }
-    this.elapsedSeconds = 0;
-  }
-
-  ngOnDestroy() {
-    this.resetTimer();
-  }
 
   cycleCourt() {
     if (this.disableNextCourtButton) return;
